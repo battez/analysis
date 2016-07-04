3376f124fc2e04d7e9345ed7328dc66339457563
battez <j.l.p.barker@dundee.ac.uk>
2016-07-04 22:14:48 +0100

    add several tweets stats functions. Entity fns prototyped

--- prepare.py
+++ prepare.py
@@ -1,4 +1,6 @@
-''' process tweets; python 3.x '''
+'''
+Process tweets; python 3.x 
+'''
 import sys
 import pymongo
 TWITDIR = 'U:\Documents\Project\scrape'
@@ -17,7 +19,9 @@ from collections import Counter
 
 
 def strip_links(text):
-    '''strip wide variety of URLs from a text'''
+    '''
+    Strip wide variety of URLs from a text
+    '''
     link_regex = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)'\
         , re.DOTALL)
     links = re.findall(link_regex, text)
@@ -27,7 +31,9 @@ def strip_links(text):
 
 
 def strip_mentions(text):
-    '''remove @mentions i.e. usernames from tweet text'''
+    '''
+    Remove @mentions i.e. usernames from tweet text
+    '''
     entity_prefixes = ['@'] # can use for hastags too, if needed
     for separator in  string.punctuation:
         if separator not in entity_prefixes :
@@ -42,7 +48,9 @@ def strip_mentions(text):
 
 
 def normalise_tweet(tweet, nums=True):
-    '''converts to lower case and cleans up the text.'''
+    '''
+    Converts to lower case and cleans up the text.
+    '''
 
     # Various regular expressions used to clean up the tweet data
     # remove_ellipsis_re = re.compile(r'[\.{2}\u2026]')
@@ -73,8 +81,10 @@ def normalise_tweet(tweet, nums=True):
 
 
 def tokenise_tweet(tweet, stemmed=False, split=True):
-    '''Pass this a pre-cleaned tweet string. We can then split it.
-    stop words removed and remainder words stemmed if required'''
+    '''
+    Pass this a pre-cleaned tweet string. We can then split it.
+    stop words removed and remainder words stemmed if required
+    '''
     #Remove the stop words.
     if split:
         tweet = tweet.strip().split()
@@ -91,7 +101,9 @@ def tokenise_tweet(tweet, stemmed=False, split=True):
 
 
 def tweet_features(tweet, split=False):
-    '''get some text features from a (normalised) tweet'''
+    '''
+    Get some text features from a (normalised) tweet
+    '''
     if split:
         tweet = tweet.split(' ')
 
@@ -103,9 +115,11 @@ def tweet_features(tweet, split=False):
     # list, dict as tuple
     return tweet_bigrams
 
-def load_tokens(bigrams='data-bigram.csv', unigrams='data-unigram.csv'):
 
-    # Load in bigrams and unigrams from spreadsheet, that we will prune dataset by.
+def load_tokens(bigrams='data-bigram.csv', unigrams='data-unigram.csv'):
+    '''
+    Load in bigrams and unigrams from spreadsheet, that we will prune dataset by.
+    '''
     from csv import reader
     invalidate_phrases = list()
     invalidate_terms = list()
@@ -122,20 +136,133 @@ def load_tokens(bigrams='data-bigram.csv', unigrams='data-unigram.csv'):
 
     return invalidate_phrases, invalidate_terms
 
+def retweet_stats(dbc):
+    ''' 
+    Return num. retweets and percentage
+    '''
+    total = dbc.count()
+    r_total = dbc.count({'text':{'$regex':'^RT'}})
+    percent = 100 * (r_total / total)
+    return r_total, float("{0:.2f}".format(percent)) 
+
+
+def reply_stats(dbc):
+    ''' 
+    Return num. replies and percentage
+    '''
+    total = dbc.count()
+    r_total = dbc.count({'original.in_reply_to_tweet_id':{'$exists':1}, \
+    'original.in_reply_to_tweet_id':{'$ne':None} })
+    percent = 100 * (r_total / total)
+    return r_total, float("{0:.2f}".format(percent))
+
+
+
+def extract_tweet_entities(tweets):
+    '''
+    FIXME: extract common entities from tweets
+    '''
+    # ref: https://dev.twitter.com/docs/tweet-entities 
+
+    if len(tweets) == 0:
+        return [], [], [], [], []
+    
+    screen_names = [ user_mention['screen_name'] 
+                         for tweet in tweets
+                            for user_mention in tweet['entities']['user_mentions'] ]
+    
+    hashtags = [ hashtag['text'] 
+                     for tweet in tweets 
+                        for hashtag in tweet['entities']['hashtags'] ]
+
+    urls = [ url['expanded_url'] 
+                     for tweet in tweets 
+                        for url in tweet['entities']['urls'] ]
+    
+    symbols = [ symbol['text']
+                   for tweet in tweets
+                       for symbol in tweet['entities']['symbols'] ]
+               
+    # In some circumstances (such as search results), the media entity
+    # may not appear
+    if tweet['entities'].has_key('media'): 
+        media = [ media['url'] 
+                         for tweet in tweets  
+                            for media in tweet['entities']['media'] ]
+    else:
+        media = []
+
+    return screen_names, hashtags, urls, media, symbols
+
+
+def get_common_entities(tweets, entity_threshold=3):
+    '''
+    Hashtags etc
+    '''
+    # Create a flat list of all tweet entities
+    tweet_entities = [  entity
+                        for tweet in tweets
+                            for entity_type in extract_tweet_entities([tweet]) 
+                                for entity in entity_type 
+                     ]
+
+    common = Counter(tweet_entities).most_common()
+
+    # Compute frequencies
+    return [ (key,val) for (key,val) in common if val >= entity_threshold ]
+
+
+# Both below: adapted from https://github.com/dandelany/tweetalyze/
+def screen_names_in_db(dbc):
+    '''
+    Returns a list of all distinct Twitter screen names in the database.
+    '''
+    return dbc.distinct('original.user.screen_name') 
+
+
+def total_tweets(dbc, threshold=1):
+    '''
+    Prints the total number of tweets for each screen name in the database.
+    [['name', '# of tweets'], ['Dee_Marketing', 1], ['YounqFlexin_Dee', 1]]
+    Can take a long time!
+    '''
+    export_data = [['name', '# of tweets']]
+    
+    for name in screen_names_in_db(dbc):
+        
+        query = dict({'original.user.screen_name': name}.items())
+        amount = dbc.find(query).count()
+        if amount > threshold:
+            export_data.append([name, amount])
+
+    return export_data
+
+
+
+##
+##
+# Process some tweets
+##
 if __name__ == '__main__':
-    ''' clean up the text - normalise tweet-text content; 
+    '''
+    clean up the text - normalise tweet-text content; 
     store tokenised and bigrams to file/db. 
-    Also prune out any tweets that have invalid terms'''
+    Also prune out any tweets that have invalid terms
+    '''
     
     # MongoDB data is from scraped tweets, so hashtag entities in original.entities
     dbc = jlpb.get_dbc('Twitter', 'rawtweets')
-    
+    print('num. distinct users; (NB original only):', len(screen_names_in_db(dbc)))
+    print(retweet_stats(dbc))
+    print(reply_stats(dbc))
+    exit()
     # for some output of results:
     from prettytable import PrettyTable
     total_num = dbc.count()
     # store a frequency tabulation using Counter()s:
     count_all = Counter()
     count_all_uni = Counter()
+    count_all_hashtags = Counter()
     num = 10 # how many to show
 
     # Get the scraped tweets from mongodb, possibly only use English (?), 
@@ -160,9 +287,11 @@ if __name__ == '__main__':
 
         phrases = tweet_features(t_tweet)
        
-        # this tallies up bigrams:
+        # this tallies up bigrams and unigrams:
         count_all.update(phrases)
         count_all_uni.update(t_tweet)
+
+                # count_all_hashtags.update()
         # we check that this tweets bigrams are not in the list of bad bigrams:
         invalidate = False
         for invalid in invalidate_phrases:
@@ -194,6 +323,11 @@ if __name__ == '__main__':
                 '$set':{'txt.normalised':n_tweet,'txt.parsed':t_tweet}\
                 })
 
+    ##
+    ##
+    # Output some stats
+    ##
+    ##
     # view our most frequent bigrams    
     common = count_all.most_common(num)
 
@@ -212,7 +346,5 @@ if __name__ == '__main__':
     # use a print wrapper to view this in case of strange non-unicode chars!
     jlpb.uprint(pt)
     
-    
-
 
     
